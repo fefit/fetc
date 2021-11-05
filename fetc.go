4f43e345bb54aa9f5a47b274c0b9b0472b7170c7
fefit <jxz_211@163.com>
2021-11-05 11:21:26 +0800

    build: update the fet version

--- fetc.go
+++ fetc.go
@@ -33,7 +33,7 @@ func main() {
 		fmt.Println("welcome to use 'fet' template engine, use 'fetc -h' for helps.")
 		return nil
 	}
-	app.Version = "0.1.6"
+	app.Version = "0.1.7"
 	err = app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
